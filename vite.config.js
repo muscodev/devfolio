f6c3c5dcf8707076947b6f3a3a6b6597332e6580
muslih_k_diy <muslihk00@gmail.com>
2025-07-06 01:08:32 +0530

    repo name is added as base path in vite conf

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import vue from '@vitejs/plugin-vue'
 import {VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/devfolio/', 
   plugins: [
+    
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
